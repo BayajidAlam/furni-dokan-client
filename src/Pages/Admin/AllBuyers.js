3bd6e2d4a869683742f15bbe440380c61aa01f7a
bayajidalam <bayajidalam2001@gmail.com>
2022-11-25 21:44:53 +0600

    admin delete functionality added

--- src/Pages/Admin/AllBuyers.js
+++ src/Pages/Admin/AllBuyers.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query'
+import { AiOutlineDelete } from "react-icons/ai";
 
 const AllBuyers = () => {
+
+  const { data:allBuyers=[],refetch } = useQuery({
+    queryKey:['allbuyers'],
+    queryFn: async () =>{
+      const res = await fetch('http://localhost:5000/allbuyers?role=buyer')
+      const data = await res.json()
+      return data;
+    }
+  })
+
+  // admin:delete a buyer 
+  function handleDelete(email) {
+    console.log(email);
+    fetch(`http://localhost:5000/deleteuser?email=${email}`, {
+      method: "DELETE",
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+
+
+      .then(res => res.json())
+      .then(data => {
+        if (data.deletedCount > 0) {
+          refetch();
+          toas
+        }
+      });
+  }
+
+
+
   return (
-    <div>
-      <h1>All Buyers</h1>
-    </div>
+    <div className="overflow-x-auto">
+    <table className="table w-full">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+      {
+        allBuyers.map((buyer,i)=>
+        <tr key={i}>
+        <th>{i+1}</th>
+        <td>{buyer.name}</td>
+        <td>{buyer.email}</td>
+        <td className="text-xl"><button onClick={()=>handleDelete(buyer.email)}><AiOutlineDelete/></button></td>
+      </tr>)
+      }
+
+ 
+      </tbody>
+    </table>
+  </div>
   );
 };
 
--- src/Pages/Admin/AllSellers.js
+++ src/Pages/Admin/AllSellers.js
@@ -1,18 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+import { useQuery } from '@tanstack/react-query'
 
 const AllSellers = () => {
 
-  const [ allSellers, setAllSellers ] = useState([])
+  const { data:allSellers=[],refetch } = useQuery({
+    queryKey:['allSellers'],
+    queryFn: async () =>{
+      const res = await fetch('http://localhost:5000/allSellers?role=seller')
+      const data = await res.json()
+      return data;
+    }
+  })
 
-  useEffect(()=>{
-    fetch('http://localhost:5000/allSellers?role=seller')
-    .then(res=>res.json())
-    .then(data=>setAllSellers(data))
-  },[])
+  // admin:delete a user 
+  const handleDelete = (email) =>{
+    console.log(email)
+        fetch(`http://localhost:5000/deleteuser?email=${email}`,{
+          method:"DELETE",
+          headers:{
+            'Content-type':'application/json'
+          } 
+        })
+
+
+      .then(res => res.json())
+      .then(data => {
+        if(data.deletedCount > 0){
+          refetch()
+          console.log(data)
+        }
+      });
+  }
+
 
   return (
-    <div>
-      <p>{allSellers.length}</p>
+    <div className="overflow-x-auto">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          allSellers.map((saller,i)=>
+          <tr key={i}>
+          <th>{i+1}</th>
+          <td>{saller.name}</td>
+          <td>{saller.email}</td>
+          <td className="text-xl"><button onClick={()=>handleDelete(saller.email)}><AiOutlineDelete/></button></td>
+        </tr>)
+        }
+
+   
+        </tbody>
+      </table>
     </div>
   );
 };
